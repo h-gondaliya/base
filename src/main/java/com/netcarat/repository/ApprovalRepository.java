001d01d32444cace03b2beb292d23c6a0ebdb017
Hemal Gondaliya <hemalgondaliya@gmail.com>
2025-10-07 12:53:06 +0530

    Create approval data

---
+++ src/main/java/com/netcarat/repository/ApprovalRepository.java
@@ -0,0 +1,9 @@
+package com.netcarat.repository;
+
+import com.netcarat.modal.Approval;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApprovalRepository extends JpaRepository<Approval, Long> {
+}
